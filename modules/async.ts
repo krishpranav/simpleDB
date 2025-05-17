409846dec1196a1a9abf945c9b2d53963ea4bb78
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-17 16:47:31 +0530

    modules: async

--- modules/async.ts
+++ modules/async.ts
@@ -12,31 +12,37 @@ export interface SuccessResult<T> {
     status: 'success';
     result: T;
 }
-
+  
 export interface ErrorResult {
     status: 'error';
     error: any;
 }
-
+  
 export type PromiseResult<T> = SuccessResult<T> | ErrorResult;
-
+  
 export async function awaitPromises<T>(promises: Promise<T>[], returnResults: boolean = true): Promise<PromiseResult<T>[]> {
     if (!promises.length) {
-        return Promise.resolve([]);
+      return Promise.resolve([]);
     }
-
+  
     const res = await Promise.all<any>(
-        promises.map(async (p) => {
-            try {
-                const res = await p;
-                if (returnResults) {
-                    return { status: 'success', result: res }
-                }
-            } catch (err) {
-                if (returnResults) {
-                    return { status: 'error', error: 'err'}
-                }
-            }
-        });
-    )
+      promises.map(async (p) => {
+        try {
+          const res = await p;
+          if (returnResults) {
+            return { status: 'success', result: res };
+          }
+        } catch (err) {
+          if (returnResults) {
+            return { status: 'error', error: err };
+          }
+        }
+      })
+    );
+  
+    if (returnResults) {
+      return res.filter((x) => x !== undefined);
+    }
+  
+    return res;
 }
